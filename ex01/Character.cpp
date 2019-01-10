5553c42f2709c4b194fd01fa892b56d47d4caf90
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-10 18:12:05 +0100

    [added] the exercise 01 is finished

---
+++ ex01/Character.cpp
@@ -0,0 +1,98 @@
+//
+// EPITECH PROJECT, 2019
+// Character.cpp
+// File description:
+// ex01
+//
+
+#include "Character.hpp"
+
+//this is constructor and the destructor
+
+Character::Character(const std::string &my_name, int my_level)
+{
+        this->name = my_name;
+        this->level = my_level;
+        this->Pv = 100;
+        this->Strength = 100;
+        this->Intelligence = 5;
+        this->Energy = 100;
+        this->Spirit = 5;
+        this->Agility = 5;
+        std::cout << this->name << " Created" << std::endl;
+}
+
+Character::~Character()
+{
+}
+
+//this is the simple getter of the code
+
+const std::string &Character::getName() const
+{
+        return(this->name);
+}
+
+int Character::getLvl() const
+{
+        return(this->level);
+}
+
+int Character::getPower() const
+{
+        return(this->Energy);
+}
+
+
+
+
+//this is the procedures
+
+int Character::CloseAttack(void)
+{
+        if (this->Energy < 10)
+                std::cout << this->name << "out of power" << std::endl;
+                else if (this->Energy > 10) {
+                        this->Energy = Energy - 10;
+                        std::cout << this->name << " strikes with a wooden stick" << std::endl;
+                }
+        return (this->Strength);
+}
+
+int Character::RangeAttack(void)
+{
+        if (this->Energy < 10)
+              std::cout << this->name << " out of power" << std::endl;
+              else if (this->Energy > 10) {
+                              this->Energy = Energy + 5;
+                              std::cout << this->name << " tosses a stone" << std::endl;
+                      }
+        return (this->Strength);
+}
+
+
+
+void Character::TakeDamage(int damage)
+{
+        this->Pv = Pv - damage;
+        if (this->Pv <= 0) {
+                this->Energy = 0;
+                std::cout << this->name << " out of combat" << std::endl;
+        } else 
+                std::cout << this->name << " takes " << damage << " damage" << std::endl;
+}
+
+void Character::Heal(void)
+{
+        this->Pv = Pv + 50;
+        if (this->Pv > 100) {
+                this->Pv = 100;
+        std::cout << this->name << " takes a potion" << std::endl;
+        }
+}
+
+void Character::RestorePower(void)
+{
+        this->Energy = 100;
+        std::cout << this->name << " eats" << std::endl;
+}
---
+++ ex01/Character.hpp
@@ -0,0 +1,51 @@
+//
+// EPITECH PROJECT, 2019
+// Character.hpp
+// File description:
+// ex01
+//
+
+#ifndef _CHARACTER_HPP_
+#define _CHARACTER_HPP_ 
+
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+class Character {
+protected:
+        private:
+                int level;
+                std::string name;
+
+                int Strength;
+                int Stamina;
+                int Intelligence;
+                int Spirit;
+                int Agility;
+                int Pv;
+                int Energy;
+
+        public:
+                Character(const std::string &name, int level);
+                ~Character();
+
+                const std::string &getName() const;
+                int getLvl() const;
+                int getPv() const;
+                int getPower() const;
+
+                int CloseAttack();
+                int RangeAttack();
+                void TakeDamage(int damage);
+                void Heal();
+                void RestorePower();
+
+        enum AttackRange {
+                CLOSE,
+                RANGE,
+        };
+        AttackRange Range;
+};
+
+#endif
